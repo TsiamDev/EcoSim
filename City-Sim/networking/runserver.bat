6351f86538d3d538fcad3974e7fae11baf652400
TsiamDev <56920806+TsiamDev@users.noreply.github.com>
2022-08-23 20:33:47 +0300

    automated Django startup

---
+++ City-Sim/networking/runserver.bat
@@ -0,0 +1,4 @@
+@echo off
+cmd /k "C:\Users\kosts\Documents\GitHub\EcoSim\Networking\EcoSimEnv\Scripts\activate.bat && cd C:\Users\kosts\Documents\GitHub\EcoSim\Networking\EcoSimSite && python manage.py runserver 192.168.1.11:80"
+pause
+"
