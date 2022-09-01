64fefc476ad184682adc8a8c5c79115ad3f36e70
TsiamDev <56920806+TsiamDev@users.noreply.github.com>
2022-09-01 22:38:39 +0300

    Updated ip

--- City-Sim/networking/RunBat.py
+++ City-Sim/networking/RunBat.py
@@ -8,5 +8,5 @@ Created on Thu Aug 25 20:08:46 2022
 import os
 
 
-os.system('cmd /k "C:\\Users\\kosts\\Documents\\GitHub\\EcoSim\\Networking\\EcoSimEnv\\Scripts\\activate.bat \&& cd C:\\Users\\kosts\\Documents\\GitHub\\EcoSim\\Networking\\EcoSimSite \&& python manage.py runserver 192.168.1.5:80"')    
+os.system('cmd /k "C:\\Users\\kosts\\Documents\\GitHub\\EcoSim\\Networking\\EcoSimEnv\\Scripts\\activate.bat \&& cd C:\\Users\\kosts\\Documents\\GitHub\\EcoSim\\Networking\\EcoSimSite \&& python manage.py runserver 192.168.1.8:80"')    
  
